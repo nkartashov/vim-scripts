24d333a8f26281e857924a31a1a466a98ff7f4f6
Nikita Kartashov <nikita.kartashov@du.co>
2017-04-12 09:14:42 +0100

    Fix cloning path

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone git@github.com:nkartashov/vim-scripts.git ~/
 RESULT_FOLDER=~/vim-scripts
-ln -s $RESULT_FOLDER/vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git@github.com:nkartashov/vim-scripts.git $RESULT_FOLDER &&
+ln -s $RESULT_FOLDER/vimrc ~/.vimrc && 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&
 echo 'Install solarized stuff yourself, will ya?'
